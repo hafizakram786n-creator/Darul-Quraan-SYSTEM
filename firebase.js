724854d09f85e0250dc0e20046dee208d6c370eb
hafizakram786n-creator <hafizakram786n@gmail.com>
2026-07-24 00:14:07 +0530

    firebase setup
    Updated Firebase SDK imports to version 12.6.0 and initialized Firestore and Auth services.

--- firebase.js
+++ firebase.js
@@ -1,20 +1,19 @@
-// firebase configuration
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCI6by4lRLmyEeH4oeP8sU_lYqETJHC10A",
-    authDomain: "darul-quraan-system.firebaseapp.com",
-    projectId: "darul-quraan-system",
-    storageBucket: "darul-quraan-system.firebasestorage.app",
-    messagingSenderId: "310086462709",
-    appId: "1:310086462709:web:6370d18062365a01c929bc"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCI6by4lRLmyEeH4oeP8sU_lYqETJHC10A",
+  authDomain: "darul-quraan-system.firebaseapp.com",
+  projectId: "darul-quraan-system",
+  storageBucket: "darul-quraan-system.firebasestorage.app",
+  messagingSenderId: "310086462709",
+  appId: "1:310086462709:web:6370d18062365a01c929bc"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+
+export { app, db, auth };
